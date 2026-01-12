9c51245986402c81186a2780dcdecfe9b18312ea
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-12 12:50:17 +0300

    added to the app.py

--- server/app.py
+++ server/app.py
@@ -13,3 +13,12 @@ def app_path():
 def index():
     host = request.headers.get('Host')
     appname = current_app.name
+    response_body = f'''
+        <h1>The host for this page is {host}</h1>
+        <h2>The name of this application is {appname}</h2>
+        <h3>The path of this application on the user's device is {g.path}</h3>
+    '''
+
+    status_code = 200
+    headers = {}
+
